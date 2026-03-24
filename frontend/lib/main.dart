70558079881880f12dd2fa5a34421c9f96344770
Maya <maya.bouban.mb@gmail.com>
2026-03-25 01:13:43 +0200

    created main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,122 +1,124 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/screens/home/home_viewmodel.dart';
+import 'package:frontend/screens/pantry/pantry_viewmodel.dart';
+import 'package:frontend/services/admin_service.dart';
+import 'package:frontend/services/pantry_service.dart';
+import 'package:frontend/services/recipe_service.dart';
+import 'package:provider/provider.dart';
+import 'package:frontend/services/api_service.dart';
+import 'package:frontend/screens/user/user_viewmodel.dart';
+import 'package:frontend/core/theme/colors.dart';
+import 'screens/home/home_screen.dart';
+import 'screens/auth/auth_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  final baseUrl = 'http://localhost:5000';
+  runApp(
+    MultiProvider(
+      providers: [
+  Provider<ApiService>(
+    create: (_) => ApiService(baseUrl: baseUrl),
+  ),
+
+  Provider<RecipeService>(
+    create: (context) => RecipeService(context.read<ApiService>()),
+  ),
+
+  Provider<PantryService>(
+    create: (context) => PantryService(context.read<ApiService>()),
+  ),
+
+  Provider<AdminService>(
+    create: (context) => AdminService(context.read<ApiService>()),
+  ),
+
+  ChangeNotifierProxyProvider3<
+      ApiService,
+      RecipeService,
+      PantryService,
+      UserViewModel>(
+    create: (context) => UserViewModel(
+      apiService: context.read<ApiService>(),
+      recipeService: context.read<RecipeService>(),
+      pantryService: context.read<PantryService>(),
+    ),
+    update: (_, api, recipeService, pantryService, vm) =>
+        vm ??
+        UserViewModel(
+          apiService: api,
+          recipeService: recipeService,
+          pantryService: pantryService,
+        ),
+  ),
+
+  ChangeNotifierProxyProvider2<ApiService, RecipeService, HomeViewModel>(
+    create: (context) =>
+        HomeViewModel(recipeService: context.read<RecipeService>()),
+    update: (_, api, recipeService, vm) =>
+        vm ?? HomeViewModel(recipeService: recipeService),
+  ),
+
+  ChangeNotifierProxyProvider3<
+      ApiService,
+      PantryService,
+      UserViewModel,
+      PantryViewModel>(
+    create: (context) => PantryViewModel(
+      pantryService: context.read<PantryService>(),
+      userViewModel: context.read<UserViewModel>(),
+    ),
+    update: (_, api, pantryService, userVM, pantryVM) =>
+        pantryVM ??
+        PantryViewModel(
+          pantryService: pantryService,
+          userViewModel: userVM,
+        ),
+  ),
+],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        primarySwatch: MaterialColor(AppColors.mainColor.toARGB32(), <int, Color>{
+          50: AppColors.mainColor.withAlpha(25),
+          100: AppColors.mainColor.withAlpha(51),
+          200: AppColors.mainColor.withAlpha(76),
+          300: AppColors.mainColor.withAlpha(102),
+          400: AppColors.mainColor.withAlpha(127),
+          500: AppColors.mainColor,
+          600: AppColors.mainColor.withAlpha(178),
+          700: AppColors.mainColor.withAlpha(204),
+          800: AppColors.mainColor.withAlpha(229),
+          900: AppColors.mainColor,
+        }),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: AppColors.mainColor,
+          primary: AppColors.mainColor,
+          secondary: AppColors.secondaryColor,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Consumer<UserViewModel>(
+        builder: (context, userViewModel, child) {
+          if (userViewModel.user != null) {
+            return  const HomeScreen();
+          } else {
+            return const AuthPage();
+          }
+        },
+      ),
+      routes: {
+        "/auth": (context) => const AuthPage(),
+        "/home": (context) => const HomeScreen(),
+      },
     );
   }
 }
